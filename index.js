1da8cae0aa45358538e9e185ee250c2af3336f85
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-03-11 12:17:42 -0600

    made default grid size 16x16, and color in cells when hovered on

---
+++ index.js
@@ -0,0 +1,32 @@
+const gridContainer = document.querySelector("#grid-container");
+const gridSizeInput = document.querySelector("#grid-size");
+const setGridSizeBtn = document.querySelector("#set-grid-size");
+const defaulGridCellPerRow = 16;
+
+
+const setGrid = (cellsPerRow) => {
+    gridContainer.innerHTML = "";
+    gridContainer.style.display = "grid";
+    gridContainer.style.gridTemplateColumns = `repeat(${cellsPerRow}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${cellsPerRow}, 1fr)`;
+
+    const totalCells = cellsPerRow * cellsPerRow;
+
+    for (let i = 0; i < totalCells; i++) {
+        const gridCell = document.createElement("div");
+        gridCell.classList.add("grid-cell");
+        gridContainer.appendChild(gridCell);
+    }
+
+    const gridCells = document.querySelectorAll(".grid-cell");
+    gridCells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "red";
+        })  
+    })
+}
+
+
+setGrid(defaulGridCellPerRow);
+
+
